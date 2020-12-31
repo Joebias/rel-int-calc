73c367120e6b906d61e1a7f4422d1bf5163f5e64
Joe Marley <joemarley1@gmail.com>
2020-12-31 14:51:44 +0000

    SSL

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,5 @@
 import '../style/index.css'
 
-var sslRedirect = require('heroku-ssl-redirect')
-var express = require('express')
-var app = express()
-
-app.use(sslRedirect())
-
-app.get('/', function (req, res) {
-    res.send('hello world')
-})
-
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
 }
