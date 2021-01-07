e292120d2eaf55a6c7bdf3cb6f4b9487891e2ee6
Joe Marley <joemarley1@gmail.com>
2021-01-07 20:48:51 +0000

    Changed RI to Relative Intensity in description

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,7 @@ export default () => {
     const [maxWeight, setMaxWeight] = useState<number>()
     console.log(defaultPlanBlueprint.maxSessionPerPeriod)
     return (
-        <PageLayout title='RI Program Generator'>
+        <PageLayout title='Relative Intensity Program Generator'>
             <div>
                 <div className='flex justify-center uppercase h-auto'>
                     <label className='mr-3 h-small w-auto'>
